82072acc75f25c9ac0caaa72e68bb6775c91cf01
Jawad Raza <jawadraza8372@gmail.com>
2023-04-14 12:33:07 +0500

    editing choose card page

--- src/Pages/ChooseCardPage.js
+++ src/Pages/ChooseCardPage.js
@@ -1,44 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CommonChooseCard from "../Components/CommonChooseCard";
-import flagsIcxon from "../ownassets/flags.png";
-import teamIcxon from "../ownassets/team.png";
-import design1 from "../ownassets/design1.png";
-import design2 from "../ownassets/design2.png";
-import design3 from "../ownassets/design3.png";
-import design4 from "../ownassets/design4.png";
-import teambtnicon from "../ownassets/teamicon.png";
-import { IoPersonOutline } from "react-icons/io5";
-import { BsFlag } from "react-icons/bs";
-import { BiImageAdd } from "react-icons/bi";
-
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Slider from "react-slick";
+import "../../node_modules/slick-carousel/slick/slick.css";
+import "../../node_modules/slick-carousel/slick/slick-theme.css";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 const ChooseCardPage = () => {
 	const navigate = useNavigate();
+	const { footballCards } = useSelector((state) => state.project);
+	const [selectedCard, setselectedCard] = useState(
+		footballCards?.length > 0 ? footballCards[0] : {}
+	);
+	useEffect(() => {
+		setselectedCard(footballCards?.length > 0 ? footballCards[0] : {});
+	}, [footballCards]);
 
-	const rawdata = [
-		{
-			id: 0,
-			title: "Design 1 hero",
-			imgSrc: design1,
-		},
-		{
-			id: 1,
-			title: "design 2 monster",
-			imgSrc: design2,
-		},
-		{
-			id: 2,
-			title: "design 3 conquror",
-			imgSrc: design3,
-		},
-		{
-			id: 3,
-			title: "design 4 devil",
-			imgSrc: design4,
-		},
-	];
-	const [firstCompNumber, setfirstCompNumber] = useState(0);
-	const [selectedCard, setselectedCard] = useState(design1);
+	const CustomLeftArrow = (props) => {
+		const { className, onClick } = props;
+
+		return (
+			<button className={className} onClick={() => onClick()}>
+				<AiOutlineLeft className='mainColor' style={{ fontSize: "20px" }} />
+			</button>
+		);
+	};
+
+	const CustomRightArrow = (props) => {
+		const { className, onClick } = props;
+
+		return (
+			<button className={className} onClick={() => onClick()}>
+				<AiOutlineRight className='mainColor' style={{ fontSize: "20px" }} />
+			</button>
+		);
+	};
+	const settings2 = {
+		dots: false,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		nextArrow: <CustomRightArrow />,
+		prevArrow: <CustomLeftArrow />,
+		responsive: [
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
 	return (
 		<div
 			className='col-10 mx-auto'
@@ -48,8 +62,64 @@ const ChooseCardPage = () => {
 				overflow: "visible",
 				background: "white",
 			}}>
-			<div style={{ margin: "50px auto" }} className='row  h-100 w-100 gx-0'>
-				<div className='col-12 col-md-3  h-100'>
+			<div
+				style={{ margin: "50px auto" }}
+				className='row d-block d-md-none h-100 w-100 gx-0'>
+				<Slider {...settings2}>
+					{footballCards &&
+						footballCards?.map((dat) => (
+							<div
+								key={dat?.id}
+								className='col-12 h-100 d-flex align-items-center flex-column justify-content-center'>
+								<img
+									style={{
+										objectFit: "contain",
+										width: "100%",
+										maxWidth: "350px",
+									}}
+									src={dat?.imgSrc}
+									alt='teams'
+								/>
+								<div
+									className='row w-100 mx-auto'
+									style={{ marginTop: "30px" }}>
+									<div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
+										<a
+											href={`${dat?.imgSrc}`}
+											style={{
+												width: "100%",
+												maxWidth: "180px",
+												textDecoration: "none !important",
+												marginBottom: "20px",
+												textTransform: "initial",
+											}}
+											download
+											// onClick={download}
+											className='btn mainColor secondarybg'>
+											Download as png
+										</a>
+									</div>
+									<div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
+										<button
+											style={{
+												width: "100%",
+												maxWidth: "180px",
+												marginBottom: "20px",
+											}}
+											onClick={() => navigate(`/cardCustomization/${dat?.id}`)}
+											className='btn mainColor thirdbg'>
+											Buy in physical
+										</button>
+									</div>
+								</div>
+							</div>
+						))}
+				</Slider>
+			</div>
+			<div
+				style={{ margin: "50px auto" }}
+				className='row d-none d-md-flex h-100 w-100 gx-0'>
+				{/* <div className='col-12 col-md-3  h-100'>
 					<CommonChooseCard title={"Personalize"}>
 						<>
 							<div className='d-flex align-items-center justify-content-center flex-column'>
@@ -175,21 +245,21 @@ const ChooseCardPage = () => {
 							</div>
 						</>
 					</CommonChooseCard>
-				</div>
-				<div className='col-12 col-md-6  h-100 d-flex align-items-center flex-column justify-content-center'>
+				</div> */}
+				<div className='col-12 col-md-8  h-100 d-flex align-items-center flex-column justify-content-center'>
 					<img
 						style={{
 							objectFit: "contain",
 							width: "100%",
 							maxWidth: "350px",
 						}}
-						src={selectedCard}
+						src={selectedCard?.imgSrc}
 						alt='teams'
 					/>
 					<div className='row w-100 mx-auto' style={{ marginTop: "30px" }}>
 						<div className='col-12 col-md-6 d-flex align-items-center justify-content-center'>
 							<a
-								href={`${design1}`}
+								href={`${selectedCard?.imgSrc}`}
 								style={{
 									width: "100%",
 									maxWidth: "180px",
@@ -210,42 +280,53 @@ const ChooseCardPage = () => {
 									maxWidth: "180px",
 									marginBottom: "20px",
 								}}
-								onClick={() => navigate("/cardCustomization/1")}
+								onClick={() =>
+									navigate(`/cardCustomization/${selectedCard?.id}`)
+								}
 								className='btn mainColor thirdbg'>
 								Buy in physical
 							</button>
 						</div>
 					</div>
 				</div>
-				<div className='col-12 col-md-3  h-100'>
+				<div className='col-12 col-md-4  h-100'>
 					<CommonChooseCard title={"Choose card"}>
 						<div className='col-12 h-100'>
-							<div className='row'>
-								{rawdata.map((dat, index) => (
-									<>
-										<div
-											onClick={() => setselectedCard(dat.imgSrc)}
-											className={`col-6 col-md-4 d-flex align-items-center justify-content-center mb-2
+							<div
+								style={{
+									width: "100%",
+									height: "450px",
+									overflowX: "hidden",
+									overflowY: "auto",
+								}}>
+								<div className='row'>
+									{footballCards &&
+										footballCards?.map((dat, index) => (
+											<>
+												<div
+													onClick={() => setselectedCard(dat)}
+													className={`col-6 col-md-4 d-flex align-items-center justify-content-center mb-2
 											}`}>
-											<div
-												style={{
-													background: "white",
-													padding: "5px",
-													cursor: "pointer",
-												}}>
-												<img
-													style={{
-														objectFit: "contain",
-														width: "100%",
-														maxWidth: "60px",
-													}}
-													src={dat.imgSrc}
-													alt={dat.title}
-												/>
-											</div>
-										</div>
-									</>
-								))}
+													<div
+														style={{
+															background: "white",
+															padding: "5px",
+															cursor: "pointer",
+														}}>
+														<img
+															style={{
+																objectFit: "contain",
+																width: "100%",
+																maxWidth: "60px",
+															}}
+															src={dat.imgSrc}
+															alt={dat.title}
+														/>
+													</div>
+												</div>
+											</>
+										))}
+								</div>
 							</div>
 						</div>
 					</CommonChooseCard>
