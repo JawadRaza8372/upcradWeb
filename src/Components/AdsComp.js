5f6e6b0955f7478481e1ed8f0fbab0f73750254f
Jawad Raza <jawadraza8372@gmail.com>
2023-04-25 18:51:59 +0500

    rechecking

--- src/Components/AdsComp.js
+++ src/Components/AdsComp.js
@@ -2,26 +2,33 @@ import React, { Component } from "react";
 import AdSense from "react-adsense";
 
 class AdsComp extends Component {
-	// componentDidMount() {
-	// 	(window.adsbygoogle = window.adsbygoogle || []).push({});
-	// }
+	componentDidMount() {
+		(window.adsbygoogle = window.adsbygoogle || []).push({});
+	}
 
 	render() {
 		return (
 			<div
 				className='allCenter'
 				style={{ width: "100%", margin: "0px auto", minWidth: "250px" }}>
+				{/* <ins
+					class='adsbygoogle'
+					style='display:block'
+					data-ad-client='ca-pub-3453611888054749'
+					data-ad-slot={`${this.props.slotnumber}`}
+					data-ad-format='auto'
+					data-full-width-responsive='true'></ins> */}
 				<AdSense.Google
 					client='ca-pub-3453611888054749'
 					slot={`${this.props.slotnumber}`}
-					// style={{
-					// 	display: "block",
-					// 	width: "100%",
-					// 	minHeight: "1px",
-					// 	minWidth: "250px",
-					// }}
-					// layout='in-article'
-					// format='fluid'
+					style={{
+						display: "block",
+						width: "100%",
+						minHeight: "1px",
+						minWidth: "250px",
+					}}
+					layout='in-article'
+					format='fluid'
 				/>
 			</div>
 		);
